6dd3fa2a407e37a70b7c200865896c7d3a79ba13
AlexandruD <AlexandruD@hotmail.com>
2022-03-15 15:23:10 +0200

    2022-03-15 saves

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -3,30 +3,32 @@ import { WindupChildren, Pace } from 'windups'
 
 function Hello() {
   return (
-    <div className="relative m-auto flex flex-1 flex-col items-center justify-center">
-      <WindupChildren>
-        <span className="text-xl text-slate-600 sm:text-lg">
-          <span className="text-2xl">{'>'}</span>{' '}
-          <span className="text-sky-700 sm:text-lg">
-            <Pace ms={40}>{' Location:'}</Pace>
+    <div className="h-full w-full pb-1">
+      <div className="relative flex flex-1 flex-col items-center justify-center pb-10">
+        <WindupChildren>
+          <span className="h-4 text-lg text-slate-600 md:h-full md:text-xl">
+            <span className="text-2xl">{'>'}</span>{' '}
+            <span className="text-sky-700 sm:text-lg">
+              <Pace ms={40}>{' Location:'}</Pace>
+            </span>
+            <Pace ms={40}>{'Bucharest, Romania'}</Pace>
+            <br />
+            <span className="text-2xl">{'>'}</span>{' '}
+            <span className="text-sky-700 sm:text-lg">
+              <Pace ms={40}>{' Email: '}</Pace>
+            </span>
+            <Pace ms={40}>{'AlexandruD@gmail.com'}</Pace>
+            <br />
+            <span className="text-2xl">{'>'}</span>{' '}
+            <span className="text-sky-700 sm:text-lg">
+              <Pace ms={40}>{' Phone: '}</Pace>
+            </span>
+            <span className="text-md">
+              <Pace ms={40}>{'+40 721 652 001'}</Pace>
+            </span>
           </span>
-          <Pace ms={40}>{'Bucharest, Romania'}</Pace>
-          <br />
-          <span className="text-2xl">{'>'}</span>{' '}
-          <span className="text-sky-700 sm:text-lg">
-            <Pace ms={40}>{' Email: '}</Pace>
-          </span>
-          <Pace ms={40}>{'AlexandruD@gmail.com'}</Pace>
-          <br />
-          <span className="text-2xl">{'>'}</span>{' '}
-          <span className="text-sky-700 sm:text-lg">
-            <Pace ms={40}>{' Phone: '}</Pace>
-          </span>
-          <span className="text-md">
-            <Pace ms={40}>{'+40 721 652 001'}</Pace>
-          </span>
-        </span>
-      </WindupChildren>
+        </WindupChildren>
+      </div>
     </div>
   )
 }
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,16 +1,19 @@
 import Hello from './Hello'
 import Image from 'next/image'
+import { BsGithub } from 'react-icons/bs'
 
 const Project = () => {
   return (
     <>
       <div className="relative m-auto flex w-full flex-1 flex-col items-center justify-center overflow-y-scroll border-b border-slate-800 p-4 scrollbar-hide">
-        <div className="mt-4 flex flex-row flex-wrap justify-around space-y-8 p-4 sm:w-full md:space-y-0 lg:space-x-20">
+        <div className="mt-4 flex w-full flex-row flex-wrap justify-around space-y-8 p-4 md:space-y-0 lg:space-x-20">
           <div className="mt-2 items-center">
             <div className="space-y-2 text-white/80">
               <p>(clone)</p>
               <h1 className="text-5xl">AirBnB</h1>
-              <h2 className="text-2xl text-sky-700">Complex Next.js project</h2>
+              <h2 className="text-2xl text-sky-700">
+                Search for the best accomodation
+              </h2>
               {/* <Hello /> */}
 
               <div className="relative m-auto flex flex-1 flex-col items-center justify-center">
@@ -34,23 +37,39 @@ const Project = () => {
             </div>
           </div>
           {/* Right side */}
-          <div className="w-96 rounded-xl border border-slate-800 p-4 shadow-xl shadow-rose-900/10 lg:w-[450px]">
+          <div className="w-96 rounded-xl border border-slate-800 p-1 shadow-xl shadow-rose-900/10 lg:w-[450px]">
             <div className="flex flex-col items-center justify-between ">
               <div className="flex w-full flex-grow items-center justify-between pb-2">
-                <span className="text-amber-500">
-                  {' '}
-                  Newest Project: <a className="text-slate-500">AirBnb clone</a>
+                <a className="ml-2 text-slate-500">AirBnb clone</a>
+
+                <span className="mr-2 text-xl text-slate-500">
+                  <a href="https://github.com/GAlexandruD/airbnb-clone">
+                    <BsGithub />
+                  </a>
                 </span>
-                <span className="text-sm text-slate-500"> 36 commits</span>
               </div>
             </div>
             <div className="w-full rounded-lg bg-[#010409] text-slate-500">
               <div className="flex space-x-6 px-2">
                 <div className=" pl-2">
-                  <img
+                  <a
+                    href="http://airbnb-clone-green-six.vercel.app/"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/static/Airbnb_Logo_BUN.webp"
+                      width="640"
+                      height="400"
+                      alt="project front page"
+                      objectFit="contain"
+                      objectPosition="center"
+                    />
+                  </a>
+
+                  {/* <img
                     src="https://dummyimage.com/600x400/2276e3/ffffff"
                     alt=""
-                  />
+                  /> */}
                 </div>
               </div>
               {/* <button className="mb-4 w-full bg-amber-600/90 text-left text-[#010409]">
@@ -75,15 +94,17 @@ const Project = () => {
           </div>
         </div>
       </div>
-      {/* End of project */}
+      {/* End of project 1*/}
 
-      <div className="relative m-auto flex w-full flex-1 flex-col items-center justify-center overflow-y-scroll scrollbar-hide">
-        <div className="mt-4 flex flex-row-reverse flex-wrap justify-around space-y-8 p-4 sm:w-full md:space-y-0 ">
+      <div className="relative m-auto flex flex-1 flex-col items-center justify-center overflow-y-scroll border-b border-slate-800 p-4 scrollbar-hide">
+        <div className="mt-4 flex w-full flex-row-reverse flex-wrap justify-around space-y-8 p-4 md:space-y-0 ">
           <div className="mt-2 items-center">
             <div className="space-y-2 text-white/80">
-              <p>(clone)</p>
-              <h1 className="text-5xl">AirBnB</h1>
-              <h2 className="text-2xl text-sky-700">Complex Next.js project</h2>
+              <p>(old project)</p>
+              <h1 className="text-5xl">Nearby Coffee Shops</h1>
+              <h2 className="text-2xl text-sky-700">
+                Discover nearby coffee shops
+              </h2>
               {/* <Hello /> */}
 
               <div className="relative m-auto flex flex-1 flex-col items-center justify-center">
@@ -94,32 +115,58 @@ const Project = () => {
                   </span>
                   <br />
                   <span className="text-2xl text-sky-700">{'>'}</span>{' '}
-                  {'Tailwind CSS for styling'}
+                  {'Foursquare API for locations'}
                   <br />
                   <span className="text-2xl text-sky-700">{'>'}</span>{' '}
-                  <span className="text-md">
-                    {'Custom map from Mapbox.com'}
-                  </span>
+                  <span className="text-md">{'Unsplash API for images'}</span>
+                  <br />
+                  <span className="text-2xl text-sky-700">{'>'}</span>{' '}
+                  <span className="text-md">{'Airtable database'}</span>
                 </span>
               </div>
-
               {/* <Hello /> */}
             </div>
           </div>
           {/* Right side */}
-          <div className="w-96 rounded-xl border border-slate-800 p-4 shadow-xl shadow-pink-700/40 lg:w-[450px]">
-            <img src="https://dummyimage.com/600x400/ba66ba/ffffff" />
-            {/* <Image
-              src="/public/static/Airbnb_Logo_BUN.webp"
-              layout="fill"
-              alt=""
-              objectFit="contain"
-              objectPosition="left"
-            /> */}
+          <div className="w-96 rounded-xl border border-slate-800 p-1 shadow-xl shadow-rose-900/10 lg:w-[450px]">
+            <div className="flex flex-col items-center justify-between ">
+              <div className="flex w-full flex-grow items-center justify-between pb-2">
+                <a
+                  className="ml-2 text-slate-500"
+                  href="http://discover-coffee-shops.vercel.app/"
+                >
+                  Discover Coffee Shops
+                </a>
+                <span className="mr-2 text-xl text-slate-500">
+                  <a href="https://github.com/GAlexandruD/discover-coffee-shops">
+                    <BsGithub />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="w-full rounded-lg bg-[#010409] text-slate-500">
+              <div className="flex space-x-6 px-2">
+                <div className=" pl-2">
+                  <a
+                    href="http://discover-coffee-shops.vercel.app/"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/static/Discover-coffee-shops.png"
+                      width="640"
+                      height="400"
+                      alt="project front page"
+                      objectFit="contain"
+                      objectPosition="center"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      {/* End of project */}
+      {/* End of project 2*/}
     </>
   )
 }
